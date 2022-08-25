8434be511d823eb908ce9cbc24ada979b8dcfd13
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-08-25 11:43:59 +0100

    adding empty query selectors for button prompts

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+const disModel = document.querySelector("");
+const nodeBackground = document.querySelector("");
+const nodeCloseBtn = document.querySelector("");
+const nodeBookModel = document.querySelector("");
+const nodeDeleteBackground = document.querySelector("");
+const nodeDeleteWarning = document.querySelector("");
+
+//Start Buttons
+const nodeAddBook = document.querySelector("");
+const nodeDeleteAllBtn = document.querySelector("");
+const nodeDeleteBtn = document.querySelector("");
+const nodeDelModelBtn = document.querySelector("");
+const nodeCancelBtn = document.querySelector("");
+const nodeTrashBtn = document.querySelector("");
+const nodeFormAddBtn = document.querySelector("");
+
+
+
 let myLibrary = [];
 
-function Book{
-	// the constructor...
+class Book() {
 	constructor(title, author, pages) {
 		this.title = title;
 		this.author = author;
 		this.pages = pages;
 		this.read = read;
 	}
+
+
+
+	function addBookToLibrary() {
+
+	}
+
 }
-
-
-function addBookToLibrary() {
-
-}
-
