7ad37081f1442be325e97ee101e63f6afa7791cc
MacauleyStephenson <MacauleyStephenson@mail.com>
2022-08-25 11:55:41 +0100

    more query slectors and constructors

--- script.js
+++ script.js
@@ -14,22 +14,30 @@ const nodeCancelBtn = document.querySelector("");
 const nodeTrashBtn = document.querySelector("");
 const nodeFormAddBtn = document.querySelector("");
 
+//
+const BookContainer = document.querySelector("");
 
+//Form element inputs
+const form = document.querySelector("");
+let titleInput = document.getElementById("");
+let authorInput = document.getElementById("");
+let pagesInput = document.getElementById("");
+let readInput = document.getElementById("");
 
-let myLibrary = [];
-
-class Book() {
-	constructor(title, author, pages) {
+//Objects array
+let elementId;
+const myLibrary = [];
+class Book {
+	arrId = (Date.now() + "").slice(-10);
+	constructor(title, author, pages, read) {
 		this.title = title;
 		this.author = author;
 		this.pages = pages;
 		this.read = read;
 	}
+}
 
 
-
-	function addBookToLibrary() {
-
-	}
+function addBookToLibrary() {
 
 }
